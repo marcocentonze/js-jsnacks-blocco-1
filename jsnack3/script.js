e0c0bbc3969fd8fa3e49b87541735ce55e7a116b
marcocentonze <marco.centonze27@gmail.com>
2023-08-29 17:26:20 +0200

    add snack(last part to complete)

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,13 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+/*Snack 3:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
 
-</head>
-<body>
-    
-    <script src="script.js"></script>
-</body>
-</html>
+/* tools:
+-array
+-sum
+-ciclo for(con possibilità che inserisca valore errato)
+-stampo
+*/
+
+
+//array
+const numberList = [];
+
+//sum
+let sum = 0;
+
+
+//ciclo for
+for (let i = 0; i <= 10; i++) { 
+  const userNumber = Number(prompt(`Inserisci il numero ${i}`));
+  console.log(userNumber);
+
+  if (isNaN(userNumber)) { //se il valore non è un numero si interrompe il ciclo
+    alert('Hai inserito un valore non accettato,aggiorna la pagina!(Sono accettati solo numeri)');
+    break;//necessario per interrompere il ciclo
+  }
+  
+  //aggiungo i numeri inseriti nell'array
+  numberList.push(userNumber); 
+  sum += userNumber;
+  console.log(sum);
+}
+
