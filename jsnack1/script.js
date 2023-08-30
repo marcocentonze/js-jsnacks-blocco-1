90d096b7f08a3cf784288a2d24511ec35c72cd60
marcocentonze <marco.centonze27@gmail.com>
2023-08-30 11:07:20 +0200

    add loop while solution

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,6 +8,7 @@ Il software stampa il maggiore. */
 -if
 */
 
+/*
 //assegno delle variabili alle risposte dell'utente tramite prompt
 const primoNum = Number(prompt("Inserisci un numero"));
 
@@ -25,4 +26,39 @@ if (primoNum > secondoNum) {
 
     risultato.innerHTML = `${secondoNum} è il numero maggiore`;
 
+} */
+
+/* Da fare con WHILE tutti I prossimi snacks
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+// esercizio rifatto con l'uso del ciclo while
+
+//dichiaro l'index
+let i = 0;
+
+//dichiaro l'array
+const utenteNumbers = [];
+
+//ciclo while
+while (i < 2) {
+    //prompt per chiedere all'utente il numero
+  const utenteNum = Number(prompt("inserisci un numero"));
+  //lo pusho nell'array
+  utenteNumbers.push(utenteNum);
+  //creo variabile per il confronto dei numeri
+  let comparisonNum = utenteNumbers[i - 1];
+
+  console.log(comparisonNum);
+
+  //if
+  if (utenteNum[i] > comparisonNum) {
+    console.log(`il numero ${utenteNumbers[i]} è maggiore di ${comparisonNum}`);
+  } else {
+    console.log(`il numero ${comparisonNum} è maggiore di ${utenteNumbers[i]}`);
+  }
+ //index di incremento (alla fine)
+  i++;
 }
+
