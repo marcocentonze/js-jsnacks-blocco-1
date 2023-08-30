5e9e606aa6fc4841dd3becfc6e7668f480c75ed1
marcocentonze <marco.centonze27@gmail.com>
2023-08-30 12:01:17 +0200

    add loop while solution

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -8,6 +8,7 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 -if    
 */
 
+/*
 //assegno delle variabili alle risposte dell'utente tramite prompt
 const primaParola = prompt("Inserisci una parola");
 
@@ -24,3 +25,42 @@ if (primaParola.length > secondaParola.length) {
 } else {
   risultatoEl.innerHTML = `${primaParola} e ${secondaParola} hanno la stessa lunghezza!`;
 }
+
+//esercizio fatto con while 
+/*  Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+// Richiedo due parole all'utente
+const parola1 = prompt("Inserisci la prima parola");
+const parola2 = prompt("Inserisci la seconda parola");
+
+let lunghezzaParola1 = parola1.length;
+let lunghezzaParola2 = parola2.length;
+
+// Stampo prima la parola più corta e poi la più lunga
+let risultato = "";
+
+// Ciclo while 
+while (lunghezzaParola1 > 0 && lunghezzaParola2 > 0) {
+
+  // Confronto le lunghezze delle parole
+  if (lunghezzaParola1 <= lunghezzaParola2) {
+    risultato += "La parola più corta è: " + parola1;
+    risultato += "La parola più lunga è: " + parola2;
+  } else {
+    risultato += "La parola più corta è: " + parola2;
+    risultato += "La parola più lunga è: " + parola1;
+  }
+  // Decremento delle lunghezze delle parole
+  lunghezzaParola1 = 0;
+  lunghezzaParola2 = 0;
+}
+//stampo in console
+console.log(risultato);
+
+
+
+
+
+
